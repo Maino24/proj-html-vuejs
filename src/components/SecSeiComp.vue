50249dc3c340ba1c85dd562fbfa585a96953f8e0
Gabriele Mainetti <mg02.gabriele@gmail.com>
2022-12-06 16:31:17 +0100

    completata section 6

--- src/components/SecSeiComp.vue
+++ src/components/SecSeiComp.vue
@@ -3,8 +3,8 @@
         <div class="container6">
             <div class="colonnaSinistra6">
                 <h5 class="our">NEWSLETTER</h5>
-                <h2>Know First</h2>
-                <div>Follow closely and receive content about our company and the news of the current market.</div>
+                <h2 class="title"><span class="word">Know</span> First</h2>
+                <div class="parag">Follow closely and receive content about our company and the news of the current market.</div>
             </div>
             <div class="colonnaDestra6">
                 <div>
@@ -45,18 +45,31 @@
         display: flex;
         justify-content: space-between;
         .our{
-            color: rgba(5, 130, 131, 1);
+            color: rgba(5, 160, 166, 1);
             font-size: 1rem;
         }
         .colonnaSinistra6{
             width: 45%;
-            
+        .title{
+            font-size: 2.5rem;
         }
+        .word{
+            background-color: rgba(23, 51, 56, 1);
+            padding: 10px 15px;
+            border-radius: 5px;
+        }
+        .parag{
+            color: rgb(170, 175, 188);
+            font-size: 1rem;
+        }
+        }
+
         .colonnaDestra6{
             width: 45%;
+            //border: 1px solid white;
             .input6{
                 width: 90%;
-                padding: 10px;
+                padding: 15px;
                 margin-top: 20px;
                 border-radius: 5px;
                 opacity: 0.5;
@@ -65,8 +78,9 @@
                 background-color: rgba(5, 130, 131, 1);
                 color: white;
                 margin-top: 20px;
-                padding: 10px 20px;
+                padding: 15px 20px;
                 border-radius: 5px;
+                margin-bottom: 0;
             }
         }
     }
